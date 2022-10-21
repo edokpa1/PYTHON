8ab7dbff001841ec7fca65c52ed70b2afd405a84
edokpa1 <abumereedokpa@gmail.com>
2022-10-21 06:06:16 -0700

    hello world

--- PYTHON OPERATORS/operate.py
+++ PYTHON OPERATORS/operate.py
@@ -4,9 +4,13 @@
 #division = 25 / 25
 #print (addition,subtraction,multi,division)
 
-my_age =27
-your_age = 30
-your_age>my_age
-my_answer = your_age>my_age
-print(my_age,your_age,my_answer)
+#my_age =27
+#your_age = 30
+#your_age>my_age
+#my_answer = your_age>my_age
+#print(my_age,your_age,my_answer)
 
+my_age = 30
+your_age =30
+my_answer = my_age==your_age
+print (my_answer)
