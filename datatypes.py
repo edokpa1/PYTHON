2e8d765204efb9c8beb3a0fbed4e381d65370c6e
edokpa1 <abumereedokpa@gmail.com>
2022-10-21 06:19:19 -0700

    operators

--- datatypes.py
+++ datatypes.py
@@ -3,5 +3,3 @@ age = 52
 accountbalance =120889832490.00
 gender = True
 print (myname,age,accountbalance,gender)
-myvar2 = "John"
-print (myvar2)
--- variable.py
+++ variable.py
@@ -1,4 +1,5 @@
 
 myday ="i wake up 5am this morning with a call from John my instructor to our classwork which is to start by 5.15 am"
 print (myday)
-print (myday) 
+print (myday)
+ 
